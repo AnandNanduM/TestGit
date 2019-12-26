0c950cf818218767d5ecd94a493a2c200ba61a71
AnandNanduM <m.anand1232@gmail.com>
2019-12-26 15:59:39 +0530

    g

---
+++ TestGit/src/adds/TestNew.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package adds;
+
+/**
+ * @author MyLife
+ *
+ */
+public class TestNew {
+
+	/**
+	 * 
+	 */
+	public TestNew() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

7c83b1bb2f5c7e60c4dd94a74f5b0a9585810490
AnandNanduM <m.anand1232@gmail.com>
2019-12-27 15:06:36 +0530

    add test files

---
+++ TestGit/src/a/Samples.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package a;
+
+/**
+ * @author User
+ *
+ */
+public class Samples {
+	/**
+	 * 
+	 */
+	public Samples() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("..>");
+	}
+}
